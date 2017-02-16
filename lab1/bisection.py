c72ee78a30d4bff940b6f39f8c0cd7ba1fdc45d5
helga-helga <olyalavyagina@gmail.com>
2017-02-16 21:37:16 +0200

    Implemented searching root of the equation using bisection method
    The interval with the root is divided in half untill reaching the desired accuracy

---
+++ lab1/bisection.py
@@ -0,0 +1,25 @@
+a = 1
+b = 1.5
+accuracy = 10**(-5)
+
+def f(x):
+    return 2 * x**5 + 3 * x**2 - 2*x - 6
+
+def bisection(a, b, accuracy):
+    c = (a + b) / 2
+    iteration = 1
+    while abs(a - b) > accuracy:
+        print 'Iteration # {}'.format(iteration)
+        print 'Approximate value {}'.format(c)
+        print 'Error: {}'.format(abs(a - b))
+        if f(c) == 0:
+            return c
+        elif f(a) * f(c) < 0:
+            b = c
+        elif f(b) * f(c) < 0:
+            a = c
+        c = (a + b) / 2
+        iteration += 1
+    return c
+
+print 'Root: {}'.format(bisection(a, b, accuracy))
