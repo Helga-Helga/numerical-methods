257166f732ed7bcc3f6bfb2b691fd7128e506986
helga-helga <olyalavyagina@gmail.com>
2017-02-17 20:04:58 +0200

    Fixed process termination condition
    Made non-strict inequality in loop

--- lab1/bisection.py
+++ lab1/bisection.py
@@ -3,7 +3,7 @@ from utils import *
 def bisection(a, b, accuracy):
     c = (a + b) / 2
     iteration = 1
-    while abs(a - b) > accuracy:
+    while abs(a - b) >= accuracy:
         output(iteration, c, abs(a - b))
         if f(c) == 0:
             return c
