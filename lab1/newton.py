85e9f4a3033e79d2f2f6deb4560737a212a9b6f6
helga-helga <olyalavyagina@gmail.com>
2017-02-26 22:02:34 +0200

    Implemented choosing of initial approximation from the ends of the segment for newton method

--- lab1/newton.py
+++ lab1/newton.py
@@ -3,6 +3,9 @@ from utils import *
 def derivativeF(x):
     return 10 * x**4 + 6 * x - 2
 
+def derivative2F(x):
+    return 40 * x**3 + 6
+
 def newton(x0, accuracy):
     iteration = 1
     while True:
@@ -13,4 +16,7 @@ def newton(x0, accuracy):
         iteration += 1
     return x0
 
-newton(b, accuracy)
+if f(a) * derivative2F(a) > 0:
+    newton(a, accuracy)
+elif f(b) * derivative2F(b) > 0:
+    newton(b, accuracy)
