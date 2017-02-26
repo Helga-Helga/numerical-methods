21551d5e9d1d7138f8344a83c3f760a61a77d085
helga-helga <olyalavyagina@gmail.com>
2017-02-26 14:34:06 +0200

    Used 2 criteria for terminating process

--- lab1/newton.py
+++ lab1/newton.py
@@ -5,9 +5,11 @@ def derivativeF(x):
 
 def newton(x0, accuracy):
     iteration = 1
-    while abs(f(x0)) >= accuracy:
+    while True:
         output(iteration, x0, abs(f(x0)))
-        x0 = x0 - f(x0) / derivativeF(x0)
+        x0_prev, x0 = x0, x0 - f(x0) / derivativeF(x0)
+        if abs(x0_prev - x0) < accuracy or abs(f(x0)) < accuracy:
+            break
         iteration += 1
     return x0
 
